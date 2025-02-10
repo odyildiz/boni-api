389cf1c917a6190d01caced3bdd10955f4b722a4
Özgür Doğan Yıldız <ozgurdoganyildiz@Ozgurs-MacBook-Air.local>
2025-02-11 00:25:39 +0300

    first commit

---
+++ src/main/java/com/bonigraphy/boni_api/BoniApiApplication.java
@@ -0,0 +1,13 @@
+package com.bonigraphy.boni_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoniApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoniApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bonigraphy/boni_api/BoniApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonigraphy.boni_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoniApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
