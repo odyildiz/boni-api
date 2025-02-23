144e99f6c1eb1e01c19c18d1aeeb3ce3fd21cf1a
odyildiz <ozgurdoganyildiz@hotmail..com>
2025-02-23 15:30:22 +0300

    add allowed origin parameters

--- src/main/java/com/bonigraphy/boni_api/auth/internal/config/CorsConfig.java
+++ src/main/java/com/bonigraphy/boni_api/auth/internal/config/CorsConfig.java
@@ -1,12 +1,18 @@
 package com.bonigraphy.boni_api.auth.internal.config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
+
+    @Value("${security.cors.allowed-origins}")
+    private List<String> allowedOrigins;
 
     @Bean
     public CorsFilter corsFilter() {
@@ -16,8 +22,10 @@ public class CorsConfig {
         // Allow credentials (cookies, authorization headers)
         config.setAllowCredentials(true);
         // Specify the exact origin instead of wildcard
-        config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedOrigin("http://localhost:3000");
+        for (String origin : allowedOrigins) {
+            config.addAllowedOrigin(origin);
+        }
+
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
